5618c85bd43bd478256049ab019e730649ec4eb6
Dario Randazzo <dario.randazzo@accedo.tv>
2026-04-09 21:10:37 -0400

    feat: improving 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,18 @@ function App()
 {
     return (
         <div id="app">
-            <PhaserGame />
-            <div className="pointer-events-none absolute inset-0 z-[1] flex items-center justify-center">
-                <ReactApp />
+            {/*
+              Letterboxed 16:9 (1920×1080) stage: max size that fits in the window while keeping aspect ratio.
+              Phaser + React overlay both use this rectangle as their coordinate space.
+            */}
+            <div
+                id="app-stage-16x9"
+                className="relative box-border overflow-hidden [height:min(100vh,calc(100vw*9/16))] [width:min(100vw,calc(100vh*16/9))]"
+            >
+                <PhaserGame />
+                <div className="pointer-events-none absolute inset-0 z-[1] flex items-center justify-center">
+                    <ReactApp />
+                </div>
             </div>
         </div>
     )
