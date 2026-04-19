f038c6648b3a4f195e1e0eb82661a52aef28a630
Dario Randazzo <dario.randazzo@accedo.tv>
2026-04-18 22:27:48 -0400

    Revert "chore: using alias imports"
    This reverts commit 3f6d3e9715061126a3098f4afdecb70698fc79b5.

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,18 +1,10 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const srcDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    resolve: {
-        alias: {
-            '#': srcDir,
-        },
-    },
     plugins: [
         react(),
     ],
--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,9 +1,5 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const srcDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../src');
 
 const phasermsg = () => {
     return {
@@ -23,11 +19,6 @@ const phasermsg = () => {
 
 export default defineConfig({
     base: './',
-    resolve: {
-        alias: {
-            '#': srcDir,
-        },
-    },
     plugins: [
         react(),
         phasermsg(),
